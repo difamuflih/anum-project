0cefab9a6e7a33e4684cec4a2a14a87b58b39b64
difamuflih <difamuflih@gmail.com>
2024-05-10 22:41:18 +0700

    revisi

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 function hitung() {
   const pinjaman = parseFloat(document.getElementById("pinjamanAwal").value);
-  const bunga = parseFloat(document.getElementById("sukuBunga").value);
+  const sukuBunga = parseFloat(document.getElementById("sukuBunga").value);
+  const bunga = sukuBunga / 100;
   const target = parseFloat(document.getElementById("target").value);
   const cicilan = parseFloat(document.getElementById("jumlahCicilan").value);
 
-  //   hitung nilai xn
-  const xn = pinjaman / cicilan + pinjaman / (bunga * cicilan);
-  document.getElementById("hasilXn").innerText = xn.toFixed(3);
+  //   hitung n
+  const n = (pinjaman * bunga) / 1 - (1 + bunga) ** -cicilan;
+  document.getElementById("hasilCicilan").innerText = n.toFixed(2);
 
-  //   hitung anuitas???????????????????????
-  const anuitas = (pinjaman * bunga) / (1 - (1 + bunga) ** -cicilan);
-  document.getElementById("hasilCicilan").innerText = anuitas.toFixed(3);
+  // hitung xn
+  const xn = n - target;
+  document.getElementById("hasilIterasi").innerText = xn.toFixed(2);
 
   // hitung faksen
   const faksen =
     (pinjaman * bunga * (1 + bunga) ** -cicilan) /
     (1 - (1 + bunga) ** -cicilan) ** 2;
-  document.getElementById("hasilAksen").innerText = faksen.toFixed(3);
-
-  // hitung fx
-  const fx = xn - target;
-  document.getElementById("hasilIterasi").innerText = fx.toFixed(3);
+  document.getElementById("hasilAksen").innerText = faksen.toFixed(2);
 }
