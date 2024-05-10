04afd02f8c07b0593b7c3ca988365004bdf5c965
difamuflih <difamuflih@gmail.com>
2024-05-10 15:33:10 +0700

    faksen

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ function hitung() {
 
   //   hitung nilai xn
   const xn = pinjaman / cicilan + pinjaman / (bunga * cicilan);
+  document.getElementById("hasilXn").innerText = xn.toFixed(3);
 
   //   hitung anuitas???????????????????????
   const anuitas = (pinjaman * bunga) / (1 - (1 + bunga) ** -cicilan);
   document.getElementById("hasilCicilan").innerText = anuitas.toFixed(3);
 
-  //   tampilkan nilai xn
-  document.getElementById("hasilCicilan").innerText = xn.toFixed(3);
+  // hitung faksen
+  const faksen =
+    (pinjaman * bunga * (1 + bunga) ** -cicilan) /
+    (1 - (1 + bunga) ** -cicilan) ** 2;
+  document.getElementById("hasilAksen").innerText = faksen.toFixed(3);
 
+  // hitung fx
   const fx = xn - target;
-
   document.getElementById("hasilIterasi").innerText = fx.toFixed(3);
 }
