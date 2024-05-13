31065156dcc7dd7acda20c6ed52f2751d37a143b
difamuflih <difamuflih@gmail.com>
2024-05-13 21:12:10 +0700

    tampilan

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function calculateAnnuity() {
   var pinjamanAwal = parseFloat(document.getElementById("pinjamanAwal").value);
   var sukuBunga = parseFloat(document.getElementById("sukuBunga").value) / 100;
-  var target = parseInt(document.getElementById("target").value);
-  var jumlahCicilan = parseInt(document.getElementById("jumlahCicilan").value);
+  var target = parseFloat(document.getElementById("target").value);
+  var jumlahCicilan = parseFloat(
+    document.getElementById("jumlahCicilan").value
+  );
 
   const cicilanAwal =
     (pinjamanAwal * sukuBunga) / (1 - (1 + sukuBunga) ** -jumlahCicilan);
@@ -22,9 +24,11 @@ function calculateAnnuity() {
   var remainingBalance =
     (pinjamanAwal * sukuBunga) / (1 - (1 + sukuBunga) ** -monthlyPayment) -
     target;
+  console.log(remainingBalance);
   var faksen =
     (pinjamanAwal * sukuBunga * (1 + sukuBunga) ** -monthlyPayment) /
     (1 - (1 + sukuBunga) ** -monthlyPayment) ** 2;
+  console.log(faksen);
 
   for (var month = 1; month <= 200; month++) {
     var newRow = document.createElement("tr");
@@ -42,8 +46,18 @@ function calculateAnnuity() {
 
     var exMonthlyPayment = monthlyPayment;
     monthlyPayment = monthlyPayment - remainingBalance / faksen;
+    console.log(monthlyPayment);
 
+    // toleransi
     if (exMonthlyPayment - monthlyPayment < 0.1) {
+      document.getElementById(
+        "hasilIterasi"
+      ).innerText = `Nilai n yang mendekati solusi optimal adalah: ${exMonthlyPayment.toFixed(
+        2
+      )} bulan`;
+      document.getElementById(
+        "hasilAksen"
+      ).innerText = `Jumlah iterasi yang dibutuhkan: ${month}`;
       break;
     }
 
@@ -68,21 +82,21 @@ function calculateAnnuity() {
       labels: labels,
       datasets: [
         {
-          label: "Sisa Pinjaman",
+          label: "Nilai x(n)",
           data: data,
           backgroundColor: "rgba(54, 162, 235, 0.2)",
           borderColor: "rgba(54, 162, 235, 1)",
           borderWidth: 1,
         },
         {
-          label: "Monthly Payment",
+          label: "Nilai f(x)",
           data: monthlyPaymentData,
           backgroundColor: "rgba(255, 99, 132, 0.2)",
           borderColor: "rgba(255, 99, 132, 1)",
           borderWidth: 1,
         },
         {
-          label: "Faksen",
+          label: "Nilai f'(x)",
           data: faksenData,
           backgroundColor: "rgba(75, 192, 192, 0.2)",
           borderColor: "rgba(75, 192, 192, 1)",
